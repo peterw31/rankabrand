dfdd399b0baa11109b9a883df3e6a71e32957578
peterw31 <peter.windemuller@gmail.com>
2015-09-13 21:33:51 +0200

    debug selection, iframe

--- RankABrand/app/js/background.js
+++ RankABrand/app/js/background.js
@@ -6,29 +6,12 @@ chrome.contextMenus.create({
   });
 
 function clickHandler(){ return function(e) {
-
-chrome.tabs.executeScript( 
-    {  code: "window.getSelection().toString();"}, 
-    function(selection) {
-        console.log("sending " + selection[0]);
-        chrome.windows.create({'url': 'rank.html?q='+encodeURIComponent(selection[0]), 'type': 'popup'}, function(window) {
-        window.onload = function(e)
-{
-alert('ok');
- window.document.getElementById("brand-search-input").value = selection[0];
-}
-   });
-
-});
-
-
-
-
-
-
-
-
-
-
-    
-};};
+        console.log("sending " + e.selectionText);
+        chrome.windows.create({ 'url': 'rank.html?q=' + encodeURIComponent(e.selectionText), 'type': 'popup' }, function (window) {
+            window.onload = function(e)
+            {
+                window.document.getElementById("brand-search-input").value = e.selectionText;
+            }
+        });
+    };
+    };
